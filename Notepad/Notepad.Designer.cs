46fb24884ea8518f7959c14dbea1af1496dc206f
MontagnaD <danrleimontagna1@gmail.com>
2024-10-22 00:03:58 -0300

    Alterado o tamanho padrão da fonte para 10

--- Notepad/Notepad.Designer.cs
+++ Notepad/Notepad.Designer.cs
@@ -183,7 +183,7 @@
             this.caixaDeTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.caixaDeTexto.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caixaDeTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.caixaDeTexto.Location = new System.Drawing.Point(12, 27);
             this.caixaDeTexto.Name = "caixaDeTexto";
             this.caixaDeTexto.Size = new System.Drawing.Size(1176, 548);
